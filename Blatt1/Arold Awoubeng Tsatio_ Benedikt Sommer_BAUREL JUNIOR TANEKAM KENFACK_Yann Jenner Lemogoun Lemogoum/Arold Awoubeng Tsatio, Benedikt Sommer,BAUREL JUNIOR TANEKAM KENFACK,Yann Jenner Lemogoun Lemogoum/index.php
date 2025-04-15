8c9a9d0e9ebc501162bae946927c20c763df41e8
Itachiyann24 <yannlemogoun@gmail.com>
2025-04-15 18:17:40 +0200

    first commit

---
+++ Blatt1/Arold Awoubeng Tsatio_ Benedikt Sommer_BAUREL JUNIOR TANEKAM KENFACK_Yann Jenner Lemogoun Lemogoum/Arold Awoubeng Tsatio, Benedikt Sommer,BAUREL JUNIOR TANEKAM KENFACK,Yann Jenner Lemogoun Lemogoum/index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+    body {
+             margin: 20;
+             background-color: 0004;
+         }
+    .container {
+        font-family: Time New Roman;
+        text-align: center;
+     } 
+
+    </style>
+    <meta charset="utf-8">
+    <title> Web 2.0 Technlogien </title>
+</head>
+<body>
+<section id="1">
+    <h1 class="container"> Übungserver Web 2.0 Technologien  </h1>
+        der Übungsgruppe sciliab- 0009.Informatik.uni-kl.de
+        hier sind unsere Abgaben aufgelistet :
+    </section>
+    <section id =2></section>
+    <ul>
+        <li>
+               <a href="/ex01/Abgabe1 "> Abgabe1</a>
+            
+        </li>
+        <li>
+              <!----<a href="/home/lamp/htdocs/ex02 "> Abgabe2</a> -->
+             leider noch nicht verfügbar 
+        </li>
+        <li>
+              <!-- <a href="/home/lamp/htdocs/ex03 "> Abgabe3</a>  -->
+              leider noch nicht verfügbar 
+            
+        </li>
+        <li>
+                <!--  <a href="/home/lamp/htdocs/ex04 "> Abgabe4</a>   -->
+                leider noch nicht verfügbar 
+            
+        </li>
+        <li>
+                <!--  <a href="/home/lamp/htdocs/ex05 "> Abgabe5</a>    -->
+                leider noch nicht verfügbar 
+        </li>
+        <li>
+               <!--   <a href="/home/lamp/htdocs/ex06 "> Abgabe6</a>     -->
+               leider noch nicht verfügbar 
+        </li>
+        <li>
+                <!--  <a href="/home/lamp/htdocs/ex07 "> Abgabe7</a>    -->
+                leider noch nicht verfügbar 
+        </li>
+        <li>
+               <!--   <a href="/home/lamp/htdocs/ex08 "> Abgabe8</a>    -->
+               leider noch nicht verfügbar 
+        </li>
+        <li>
+              <!--    <a href="/home/lamp/htdocs/ex09 "> Abgabe9</a>    -->
+              leider noch nicht verfügbar 
+        </li> 
+        <li>
+              <!--    <a href="/home/lamp/htdocs/ex10 "> Abgabe10</a>   -->
+              leider noch nicht verfügbar 
+        </li>
+        <li>
+              <!--    <a href="/home/lamp/htdocs/ex11 "> Abgabe11</a> -->
+              leider noch nicht verfügbar 
+        </li>
+        <li>
+               <!--   <a href="/home/lamp/htdocs/ex12 "> Abgabe12</a>   -->
+               leider noch nicht verfügbar 
+        </li>
+    </section>
+    <section id="3">
+    </ul>
+    and der Bearbeitung der Abgaben haben mitgearbeitet:
+    <br>
+    <ul> 
+    <li>
+        Arold AWOUBENG TSATIO  
+    </li>
+    <li>BAUREL JUNIOR TANEKAM KENFACK</li>
+    <li>Benedikt Sommer  	 	    </li>
+    <li>Yann Jenner Lemogoun Lemogoum</li>
+    </ul>
+</section>
+Servername <?php
+   echo gethostname();
+  ?>.informatik.uni-kl.de
+</body>
+</html>
+
